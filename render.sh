612356c75cc93e7d87aa46449398b5c168f66ca1
Gary Klindt <gary.klindt@googlemail.com>
2024-01-22 21:39:44 +0100

    Add diagram, improve wording, spelling and grammar

--- render.sh
+++ render.sh
@@ -8,5 +8,5 @@ rm */*.html
 python3.12 scripts/main.py
 
 # categories and posts are rendered
-asciidoctor -a stylesheet=style.css -a stylesdir=$(pwd) -a docinfodir=$(pwd)/docinfo -a docinfo=private-footer,private-header posts/*.adoc
+asciidoctor -a stylesheet=style.css -a stylesdir=$(pwd) -a docinfodir=$(pwd)/docinfo -a docinfo=private-footer,private-header -r asciidoctor-diagram posts/*.adoc
 asciidoctor -a stylesheet=style.css -a stylesdir=$(pwd) -a docinfodir=$(pwd)/docinfo -a docinfo=private-footer,private-header categories/*.adoc *.adoc
